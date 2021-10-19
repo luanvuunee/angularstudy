2a5dbb351e2031118e2238f4bf812a8492899ead
unknown <luanvuforwork@gmail.com>
2021-10-19 18:08:32 +0700

    DoneAddStudent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { DasboardComponent } from './dasboard/dasboard.component';
 import { HomeComponent } from './home/home.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { StudentFormComponent } from './student-form/student-form.component';
+import { StudentsComponent } from './students/students.component';
 
 const routes: Routes = [
-  {path: '', component: AboutComponent},
-  {path: 'home', component:HomeComponent},
+  {path: '', component: DasboardComponent},
+  {path: 'dashboard', component: DasboardComponent},
+  // {path: 'home', component:HomeComponent},
   {path: 'about', component:AboutComponent},
-  {path: 'login', component:LoginFormComponent},
+  {path: 'students', component:StudentsComponent},
+  {path: 'student-form', component:StudentFormComponent},
+  // {path: 'login', component:LoginFormComponent},
   {path: '**', component: NotFoundComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,47 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import { StudentsComponent } from './students/students.component';
+import { DasboardComponent } from './dasboard/dasboard.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { StudentFormComponent } from './student-form/student-form.component';
 
+import {MatInputModule} from '@angular/material/input';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import {MatMenuModule} from '@angular/material/menu';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTabsModule} from '@angular/material/tabs';
+
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatTreeModule} from '@angular/material/tree';
 
 
 
@@ -28,7 +67,10 @@ import {MatListModule} from '@angular/material/list';
     AboutComponent,
     NotFoundComponent,
     FontBoldDirective,
-    LoginFormComponent
+    LoginFormComponent,
+    StudentsComponent,
+    DasboardComponent,
+    StudentFormComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +82,12 @@ import {MatListModule} from '@angular/material/list';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    
+    
     
   ],
   providers: [],
